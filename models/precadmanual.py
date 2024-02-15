1d67f78b0c9f4cd5beccb79e2fb57c840d42d643
supnatal <magnoodilon60@gmail.com>
2024-02-15 16:30:36 -0300

    feat: projeto pagina com streamlit | python | Mysql

---
+++ models/precadmanual.py
@@ -0,0 +1,13 @@
+
+
+
+
+class preCadastroManual:
+    def __init__(self, NumPac, DataColeta, HoraColeta, DataEntrega, HoraEntrega,  DataFatura, DataEntregaApoio):
+        self.NumPac = NumPac
+        self.DataColeta = DataColeta
+        self.HoraColeta = HoraColeta
+        self.DataEntrega = DataEntrega
+        self.HoraEntrega = HoraEntrega
+        self.DataFatura = DataFatura
+        self.DataEntregaApoio = DataEntregaApoio
