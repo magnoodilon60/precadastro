8027f4852d1e374761bf318d381fb5e3d0928b12
supnatal <magnoodilon60@gmail.com>
2024-02-16 17:17:23 -0300

    feat: realiado ajustes no login

--- main.py
+++ main.py
@@ -2,16 +2,52 @@ from datetime import datetime
 import streamlit as st
 import controllers.precadControllers as precadControllers
 import models.precadmanual as precadastro
-import services.secrets as secrets
-
+import hmac
 
 # No meia o titulo da pagina
 st.set_page_config(
     page_title="Ajuste Pré-cadaastro")
 
 # chama a função de login
-secrets.check_password()
+# secrets.check_password()
 
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 #titulo central da pagina
 st.title('Ajustar Pré-cadastro de Pacientes')
